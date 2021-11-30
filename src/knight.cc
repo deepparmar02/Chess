dece8c70689d8bf1336435ba53e3ac7b823b33cc
Vansh <https://github.com/vansh844>
2021-11-30 18:12:46 -0500

    king, rook, knight and bishop done

--- src/knight.cc
+++ src/knight.cc
@@ -15,5 +15,20 @@ bool Knight::isMoved(){
 } 
 
 bool Knight::isValidMove(int startRow, int startCol, int endRow, int endCol, Piece* board[8][8]){
-    return true;
+    if(endCol == endCol + 1 || endCol == endCol - 1){
+        if(endRow == startRow + 2 || endRow == startRow - 2){
+            if(board[endRow][endCol]->getColour() != colour){
+                return true;
+            }
+            return false;
+        }
+    }else if(endCol == endCol + 2 || endCol == endCol - 2){
+        if(endRow == startRow + 1 || endRow == startRow - 1){
+            if(board[endRow][endCol]->getColour() != colour){
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
 }
--- src/rook.cc
+++ src/rook.cc
@@ -15,5 +15,25 @@ bool Rook::isMoved(){
 } 
 
 bool Rook::isValidMove(int startRow, int startCol, int endRow, int endCol, Piece* board[8][8]){
-    return true;
+    if(board[endRow][endCol]->getColour() == colour){
+        return false;
+    }
+    if(startRow == endRow){ // Checks if there is a piece between startCol and endCol (horizontally)
+        int moveDir = (endCol - startCol > 0) ? 1 : -1; // 1 is right 
+        for(int i = startCol + moveDir; i != endCol; i += moveDir){
+            if(board[startRow][i]->getType() != Empty){
+                return false;
+            }
+        }
+        return true;
+    }else if(startCol == endCol){ // Checks if there is a piece between startRow and endRow (vertically)
+        int moveDir = (endRow - startRow > 0) ? 1 : -1; // 1 is right 
+        for(int i = startRow + moveDir; i != endRow; i += moveDir){
+            if(board[i][startCol]->getType() != Empty){
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
 }
