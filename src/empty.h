7d43eeadc863a5a2b6145c1025df142459125325
Vansh <https://github.com/vansh844>
2021-11-30 16:44:15 -0500

    empty fixed

--- src/empty.h
+++ src/empty.h
@@ -4,10 +4,8 @@
 
 class Empty: public Piece {
     public:
-        Empty(PieceColour colour);
         PieceType getType() override;
         isValidMove(int startRow, int startCol, int endRow, int endCol, Piece* board[8][8]) override;
-        PieceColour getColour() override;
         bool isMoved() override;
 
 }
