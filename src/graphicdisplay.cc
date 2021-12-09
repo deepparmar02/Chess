a637a6c7ef36751dc5830f4dc70c5234a0fc5e89
Deep Parmar <https://github.com/deepparmar02>
2021-12-08 22:27:09 -0500

    Displayed pieces as text

--- src/graphicdisplay.cc
+++ src/graphicdisplay.cc
@@ -1,8 +1,10 @@
 #include "graphicdisplay.h"
 #include "window.h"
 #include "board.h"
+#include <string>
 
 const int SQUARE_SIZE = 50;
+const int PIECE_SIZE = 100;
 const int BOARD_SIZE = 8;
 
 GraphicDisplay::GraphicDisplay(Board *board_subject) : 
@@ -15,12 +17,12 @@ GraphicDisplay::GraphicDisplay(Board *board_subject) :
 void GraphicDisplay::notify() {
     for (int i = 0; i < BOARD_SIZE; ++i) {
         for (int j = 0; j < BOARD_SIZE; ++j) {
-            
+
             int colour;
             if ((i + j) % 2 == 0) {
                 colour = Xwindow::White;
             } else {
-                colour = Xwindow::Black;
+                colour = Xwindow::Green;
             }
 
             screen.fillRectangle(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, colour);
@@ -29,10 +31,31 @@ void GraphicDisplay::notify() {
             char file = 'a' + i;
 
             auto piece = board_subject->getPieceAt(file, rank);
+            auto pieceColour = piece->getColour();
+            auto pieceType = piece->getType();
+            char pieceLetter;
 
-            // here, we do some logic. We draw the piece depending on
-            // what type of piece and colour it is. Haven't done that yet. We can all work on it together.
+            if (pieceType == Piece::PieceType::King) {
+                pieceLetter = 'K';
+            } else if (pieceType == Piece::PieceType::Queen) {
+                pieceLetter = 'Q';
+            } else if (pieceType == Piece::PieceType::Bishop) {
+                pieceLetter = 'B';
+            } else if (pieceType == Piece::PieceType::Rook) {
+                pieceLetter = 'R';
+            } else if (pieceType == Piece::PieceType::Knight) {
+                pieceLetter = 'N';
+            } else if (pieceType == Piece::PieceType::Pawn) {
+                pieceLetter = 'P';
+            } else {
+                pieceLetter = ' ';
+            }
 
+            if (pieceColour == Piece::PieceColour::Black) {
+                pieceLetter = tolower(pieceLetter);
+            }
+
+            screen.drawString(i * SQUARE_SIZE + SQUARE_SIZE / 2, j * SQUARE_SIZE + SQUARE_SIZE / 2, std::string(1, pieceLetter));
         }
     }
 }
