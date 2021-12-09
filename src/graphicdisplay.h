baca6c844df97308b85e2e7081c82457ff7610e4
lntran <lntran@student.cs.uwaterloo.ca>
2021-12-09 17:11:36 -0500

    Drew the pieces on Xwindow

--- src/graphicdisplay.h
+++ src/graphicdisplay.h
@@ -2,6 +2,7 @@
 #define GRAPHIC_DISPLAY_H
 #include "observer.h"
 #include "window.h"
+#include "piece.h"
 #include <memory>
 class Board;
 
@@ -14,6 +15,12 @@ class GraphicDisplay : public Observer {
         Board *board_subject;
         // what else to add? maybe XWindow maybe?
         Xwindow screen;
+        void drawRook(int x, int y, int colour);
+        void drawPawn(int x, int y, int colour);
+        void drawBishop(int x, int y, int colour);
+        void drawKnight(int x, int y, int colour);
+        void drawQueen(int x, int y, int colour);
+        void drawKing(int x, int y, int colour);
 };
 
 #endif
